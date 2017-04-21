d50baed76586c9d39dd0f5c8bd7851e7ff9fa994
Robert Fairhead <bob@adhocteam.us>
2017-04-20 21:54:47 -0400

    Make it build

--- scripts/run_update.sh
+++ scripts/run_update.sh
@@ -1,12 +1,5 @@
 #! /usr/bin/env bash
 
-# Get to git repo root directory from the directory of this script
-cd $(dirname $0)/..
-
-# Get the latest version locally
-git checkout master
-git pull
-
 # Create branch for the update using data to differentiate
 git checkout -b "$(date -I)-ga-data"
 
@@ -19,24 +12,3 @@ git commit -m "$(date -I) automated GA data pull"
 git push -u origin HEAD
 git checkout master
 git branch -d "$(date -I)-ga-data"
-
-GH_TOKEN=$( git config --get github.token )
-
-# Open the pull request and store its number
-ISSUE="$(curl -H "Authorization: token $GH_TOKEN" \
-     --data "{
-              \"title\": \"$(date -I) automated GA data pull\",
-              \"body\": \"Weekly GA data pull\",
-              \"head\": \"$(date -I)-ga-data\",
-              \"base\": \"master\"
-            }" \
-     https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/pulls \
-     2> /dev/null | grep \"number\": | sed 's/^\W*number\W*\([0-9][0-9]*\)\W*/\1/')"
-
-# Assign the pull request so an email notification gets sent
-curl -H "Authorization: token $GH_TOKEN" \
-    --data "{
-             \"assignee\": \"$(git config --get github.username)\"
-           }" \
-    -X PATCH \
-    https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/issues/$ISSUE
