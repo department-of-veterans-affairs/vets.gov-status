d88087421d267b923a3821cccc212e923d00a201
Robert Fairhead <bob@adhocteam.us>
2017-05-30 18:48:26 -0400

    Temporarily disable writing to github

--- scripts/run_update.sh
+++ scripts/run_update.sh
@@ -38,29 +38,29 @@ cd ..
 rm -rf update_data
 
 # Push our changes up to github and clean up local branch
-git add .
-git commit -m "$(date -I) automated GA data pull"
-git push -u origin HEAD
-git checkout master
-git branch -d "$(date -I)-ga-data"
-
-GH_TOKEN=$( git config --get github.token )
-
-# Open the pull request and store its number
-ISSUE="$(curl -H "Authorization: token $GH_TOKEN" \
-     --data "{
-              \"title\": \"$(date -I) automated GA data pull\",
-              \"body\": \"Daily GA data pull\",
-              \"head\": \"$(date -I)-ga-data\",
-              \"base\": \"master\"
-            }" \
-     https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/pulls \
-     2> /dev/null | grep \"number\": | sed 's/^\W*number\W*\([0-9][0-9]*\)\W*/\1/')"
-
-# Assign the pull request so its easier to find
-curl -H "Authorization: token $GH_TOKEN" \
-    --data "{
-             \"assignee\": \"$(git config --get github.username)\"
-           }" \
-    -X PATCH \
-    https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/issues/$ISSUE
+# git add .
+# git commit -m "$(date -I) automated GA data pull"
+# git push -u origin HEAD
+# git checkout master
+# git branch -d "$(date -I)-ga-data"
+#
+# GH_TOKEN=$( git config --get github.token )
+#
+# # Open the pull request and store its number
+# ISSUE="$(curl -H "Authorization: token $GH_TOKEN" \
+#      --data "{
+#               \"title\": \"$(date -I) automated GA data pull\",
+#               \"body\": \"Daily GA data pull\",
+#               \"head\": \"$(date -I)-ga-data\",
+#               \"base\": \"master\"
+#             }" \
+#      https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/pulls \
+#      2> /dev/null | grep \"number\": | sed 's/^\W*number\W*\([0-9][0-9]*\)\W*/\1/')"
+#
+# # Assign the pull request so its easier to find
+# curl -H "Authorization: token $GH_TOKEN" \
+#     --data "{
+#              \"assignee\": \"$(git config --get github.username)\"
+#            }" \
+#     -X PATCH \
+#     https://api.github.com/repos/department-of-veterans-affairs/vets.gov-status/issues/$ISSUE
